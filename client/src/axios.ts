a82a133c860ca4299287e9397bc84cdafa8db293
Gavryk <gvozdyk23@gmail.com>
2023-03-07 02:42:09 +0200

    client: axois added some options to instance

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -4,4 +4,9 @@ const instance = axios.create({
 	baseURL: process.env.REACT_APP_BASE_URL,
 });
 
+instance.interceptors.request.use((config) => {
+	config.headers!.Authorization = window.localStorage.getItem('user');
+	return config;
+});
+
 export default instance;
