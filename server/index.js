81852b442916b23c5854763236db41abc0b52b2e
Gavryk <gvozdyk23@gmail.com>
2023-03-10 01:00:03 +0200

    server: add image on server with multer

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import * as fs from 'fs';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import multer from 'multer';
 import cookieParser from 'cookie-parser';
 import { loginValidator, registerValidator } from './validations.js';
 import { handleValidationErrors } from './utils/index.js';
@@ -9,19 +11,42 @@ import { register, login } from './controllers/UserController.js';
 
 //.env config
 dotenv.config({ debug: true });
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
 mongoose
 	.connect(process.env.MONGO_DB)
 	.then(() => console.log('DB ok'))
 	.catch((err) => console.log('DB error', err));
 
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/uploads', express.static('/uploads'));
+
+//Upload Storage
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		if (!fs.existsSync('uploads')) {
+			fs.mkdirSync('uploads');
+		}
+		cb(null, 'uploads');
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + file.originalname.replaceAll(' ', '_'));
+	},
+});
+const upload = multer({ storage });
+
 //Routes
 //Auth
 app.post('/auth/register', registerValidator, handleValidationErrors, register);
 app.post('/auth/login', loginValidator, handleValidationErrors, login);
+
+//Upload Image Route
+app.post('/upload', upload.single('image'), (req, res) => {
+	res.json({
+		url: `/uploads/${req.file.filename}`,
+	});
+});
 
 app.listen(process.env.PORT || 4040, (err) => {
 	if (err) {
