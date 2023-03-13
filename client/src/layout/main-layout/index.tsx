85398aec80d9002439624bee2ee986ba0b41909e
Gavryk <gvozdyk23@gmail.com>
2023-03-14 00:59:21 +0200

    ui: layout changes

--- client/src/layout/main-layout/index.tsx
+++ client/src/layout/main-layout/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 export const MainLayout: React.FC = () => {
+	const { pathname } = useLocation();
+	const path = pathname.replace('/', '');
 	return (
 		<div
 			className={`app h-screen bg-gradient-to-br from-black to-[#121286] hide-scrollbar flex items-center`}>
-			<div className="xl:container mx-auto">
+			<div
+				className={`mx-auto ${
+					path !== 'login' && path !== 'register'
+						? 'bg-white bg-opacity-15 min-[1250px]:w-3/4 min-[576px]:w-5/6 min-[320px]:w-11/12 flex h-[94%] my-3 rounded-[15px]'
+						: 'xl:container'
+				}`}>
 				<Outlet />
 			</div>
 		</div>
