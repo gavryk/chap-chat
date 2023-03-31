67b5f3e8bd5e47a7e713e41cd8cd3de8912a2af1
Gavryk <gvozdyk23@gmail.com>
2023-03-31 20:42:11 +0300

    ui: changed useAvatarHolder hook for ui-avatar rfc

--- client/src/components/ui-avatar/model/hooks/useAvatarHolder.ts
+++ client/src/components/ui-avatar/model/hooks/useAvatarHolder.ts
@@ -1,23 +1,33 @@
 import { useMemo } from 'react';
-export const useAvatarHolder = (userId: string, alt: string) => {
-	const colors = [
-		'bg-teal-200',
-		'bg-red-200',
-		'bg-green-200',
-		'bg-purple-200',
-		'bg-blue-200',
-		'bg-yellow-200',
-		'bg-orange-200',
-		'bg-pink-200',
-		'bg-fuchsia-200',
-		'bg-rose-200',
-	];
+
+type AvatarColorMap = Record<string, string>;
+
+const AvatarColor: AvatarColorMap = {
+	teal: 'bg-teal-200',
+	red: 'bg-red-200',
+	green: 'bg-green-200',
+	purple: 'bg-purple-200',
+	blue: 'bg-blue-200',
+	yellow: 'bg-yellow-200',
+	orange: 'bg-orange-200',
+	pink: 'bg-pink-200',
+	fuchsia: 'bg-fuchsia-200',
+	rose: 'bg-rose-200',
+};
+
+const calculateColor = (userId: string, alt: string): string => {
 	const userIdBase10 = userId
 		? parseInt(userId.substring(10), 16)
 		: parseInt(alt.substring(10), 16);
+
+	const colors = Object.values(AvatarColor);
 	const colorIndex = userIdBase10 % colors.length;
-	const color = colors[colorIndex];
-	return useMemo(() => {
-		return color;
-	}, [userId]);
+
+	return colors[colorIndex];
+};
+
+export const useAvatarHolder = (userId: string, alt: string) => {
+	const color = useMemo(() => calculateColor(userId, alt), [userId]);
+
+	return color;
 };
